1b81f41df142ed5e70b9231a81d8235285b648aa
Freddy Cabrera <fredjhonas2016@gmail.com>
2022-02-13 00:23:15 -0300

    Agrega vista-timeline inicial

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -1,12 +1,35 @@
 // import styles from './Timeline.module.css'
-
 // import Head from "next/head";
+import Image from "next/image";
+import Card from "@material-tailwind/react/Card";
 
-const Timeline = () => {
+const Timeline = ({ timeLines }) => {
   return (
-    <>
-      <h1 className="text-2xl">Timeline</h1>
-    </>
+    <section className="lg:p-16 md:p-16">
+      <Card className="p-10">
+        {timeLines.length > 0 &&
+          timeLines.map((timeLine: any, index: number) => {
+            const { avatar, username, message } = timeLine;
+            return (
+              <article className="flex pb-16" key={index}>
+                <div>
+                  <Image
+                    src={avatar}
+                    layout="fixed"
+                    height={45}
+                    width={45}
+                    className="rounded-full"
+                  />
+                </div>
+                <div>
+                  <strong>{username}</strong>
+                  <p>{message}</p>
+                </div>
+              </article>
+            );
+          })}
+      </Card>
+    </section>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 // import styles from "./Home.module.css";
+import { useState, useEffect } from "react";
 import Timeline from "components/Timeline";
 import PrivateRoute from "components/PrivateRoute";
 
 const Home = () => {
-  return <Timeline />;
+  const [timeLine, setTimeline] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/statuses/home_timeline")
+      .then((res) => res.json())
+      .then(setTimeline);
+  }, []);
+
+  return <Timeline timeLines={timeLine} />;
 };
 
 export default PrivateRoute(Home);
